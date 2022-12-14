79ed0ab26edc88ef52bc54f33562b993dae8bb90
Simbarashe Mashavave <mashavavesimbarashe@gmail.com>
2022-12-14 14:28:05 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints _putchar and add a new line
+ *
+ * return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
