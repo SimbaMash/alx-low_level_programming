272781af947ced374b98c2282eb57f297ebc5c2d
Simbarashe Mashavave <mashavavesimbarashe@gmail.com>
2022-12-16 14:44:39 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ *
+ * @n: the integer being computed
+ *
+ * Return: Always 0 (Success)
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
