d2d233c97935668d265aab2fa275add4a56b7b45
Simbarashe Mashavave <mashavavesimbarashe@gmail.com>
2022-12-16 02:25:35 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphebet in lowercase
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for(ch='a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+
+	return (0);
+}
