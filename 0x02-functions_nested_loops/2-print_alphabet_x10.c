3b387c8e622a4f8b02371e82cae49886e45f989a
Simbarashe Mashavave <mashavavesimbarashe@gmail.com>
2022-12-16 02:45:49 -0800

    corrected code

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet ten times
+ * followed by a new line
+*/
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char ch;
+
+	while (count++ <= 9)
+	{
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	
+	_putchar('\n');
+	}
+}
