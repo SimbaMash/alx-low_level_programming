994fcdadf5066edd0dc154c8b91549696046be7d
Simbarashe Mashavave <mashavavesimbarashe@gmail.com>
2022-12-16 15:27:41 -0800

    trying a new interger

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * @a: the first interger to be added
+ * @b: the second interger to be added
+ * @c: the sum of a and b
+ *
+*/
+
+int add(int a, int b)
+{
+	int c = a +b;
+
+	return (c);
+}
