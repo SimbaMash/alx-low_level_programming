4485d638782d23655dfff9101d388cc1f295f2ea
Simbarashe Mashavave <mashavavesimbarashe@gmail.com>
2022-12-16 02:01:10 -0800

    Header file and corrections

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Description: This is the header file that contains
+ * all prototypes and function declarations for the
+ * project
+*/
+
+
+int _putchar(char c);
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
