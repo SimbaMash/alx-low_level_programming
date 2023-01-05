d888ff470df50bca127832466d5bbd006c914a65
Simbarashe Mashavave <mashavavesimbarashe@gmail.com>
2023-01-05 13:28:23 -0800

    primal

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - prints an interger
+ * @n: the interger to be printed
+*/
+
+
+void print_number(void)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
+}
