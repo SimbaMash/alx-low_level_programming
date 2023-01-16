f20f8bba278169bb306e3f99d5618a74e0bb083a
Simbarashe Mashavave <mashavavesimbarashe@gmail.com>
2023-01-16 04:09:58 -0800

    write in c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Prints out the quote without using printf and put
+ *
+ * Result: 1 if it ran successfully
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	result (1);
+}
