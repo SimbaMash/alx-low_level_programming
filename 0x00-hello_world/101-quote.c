f2b8979394048398971710b0a238f6f64907069e
Simbarashe Mashavave <mashavavesimbarashe@gmail.com>
2023-01-16 04:13:56 -0800

    changing the result

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  * main - Prints out the quote without using printf and put
  *
- * Result: 1 if it ran successfully
+ * Return: 1 if it ran successfully
 */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	result (1);
+	return (1);
 }
