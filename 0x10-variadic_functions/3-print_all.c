de161d787b00d33c49134aa944c385a241985686
Simbarashe Mashavave <mashavavesimbarashe@gmail.com>
2023-01-30 13:28:23 +0200

    catching up

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,92 @@
-#include "variadic_functions.h"
+#include <stdarg.h>
 #include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_c - print a char
+ * @c: char to print
+ *
+ * Return: void
+ */
+void print_c(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+
+/**
+ * print_s - prints a string
+ * @s: string to print
+ *
+ * Return: void
+ */
+void print_s(va_list s)
+{
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
+ * print_i - prints an int
+ * @i: int to print
+ *
+ * Return: void
+ */
+void print_i(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+ * print_f - prints a float
+ * @f: float to print
+ *
+ * Return: void
+ */
+void print_f(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
 
 /**
  * print_all - prints anything
- * @format: list of types of arguments passed to the function
+ * @format: list of argument types passed to the function
+ *
+ * Return: void
  */
 void print_all(const char * const format, ...)
 {
-	int i, result;
-	char *str;
-	va_list ap;
+	unsigned int i, j;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"f", print_f},
+		{NULL, NULL}
+	};
+	va_list valist;
+	char *separator = "";
 
-	va_start(ap, format);
+	va_start(valist, format);
 	i = 0;
-	while (format != NULL && format[i] != '\0')
+	while (format && format[i])
 	{
-		switch (format[i])
+		j = 0;
+		while (p[j].t != NULL)
 		{
-		case 'c':
-			printf("%c", va_arg(ap, int));
-			result = 0;
-			break;
-		case 'i':
-			printf("%d", va_arg(ap, int));
-			result = 0;
-			break;
-		case 'f':
-			printf("%f", va_arg(ap, double));
-			result = 0;
-			break;
-		case 's':
-			str = va_arg(ap, char *);
-			if (str == NULL)
-				printf("(nil)");
-			printf("%s", str);
-			result = 0;
-			break;
-		default:
-			result = 1;
-			break;
+			if (*(p[j].t) == format[i])
+			{
+				printf("%s", separator);
+				p[j].f(valist);
+				separator = ", ";
+				break;
+			}
+			j++;
 		}
-		if (format[i + 1] != '\0' && result == 0)
-			printf(", ");
 		i++;
 	}
+	va_end(valist);
 	printf("\n");
-	va_end(ap);
 }
